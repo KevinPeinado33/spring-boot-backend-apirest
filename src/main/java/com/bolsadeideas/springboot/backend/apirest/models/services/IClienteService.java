274ab0e0bbb748cd91cba27ac49a902df814963c
KevinPeinado <kevinpeinado@upeu.edu.pe>
2021-01-11 09:50:16 -0500

    ultimo cambio en el backend

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/services/IClienteService.java
@@ -2,12 +2,17 @@ package com.bolsadeideas.springboot.backend.apirest.models.services;
 
 import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Region;
 
 public interface IClienteService {
 	
 	public List<Cliente> findAll();
+	
+	public Page<Cliente> findAll(Pageable pageable);
 	
 	public Cliente findById(Long id);
 	
